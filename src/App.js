961c63f3b2bbceba62e77a3249ab6e97498d076e
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-11-15 01:59:41 +0200

    add previous solving tasks from HW 2

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import { v4 } from 'uuid';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const { contacts } = this.state;
+    const contact = {
+      id: v4(),
+      name,
+      number,
+    };
+    contacts.map(contact => contact.name).includes(name)
+      ? alert(`Inputed ${name} is already in the contacts`)
+      : this.setState({ contacts: [contact, ...contacts] });
+  };
+
+  handleFilterContacts = e => {
+    return this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normaziledFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normaziledFilter),
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filtredContacts = this.getFilteredContacts();
+    return (
+      <div className="main_container">
+        <h1 className="main_title">Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2 className="title">Contacts</h2>
+        <Filter filter={filter} onFilterChange={this.handleFilterContacts} />
+        <ContactList
+          contacts={filtredContacts}
+          handleDelete={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+export default App;
